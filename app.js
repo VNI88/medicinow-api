f417982881ad2d8f4290094855d3b77bc30d31ce
VInicius Cavalcante Leite <vini_cavalcanteee@hotmail.com>
2019-06-20 23:09:48 -0300

    POST appointments and GET one appointment

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import db from './db/db';
-
+import bodyParser from 'body-parser';
 //Setting express app
 const app = express();
+
+//Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //get all appointments
 app.get('/api/v1/appointments', (req, res) => {
@@ -12,6 +16,46 @@ app.get('/api/v1/appointments', (req, res) => {
     appointments: db
   })
 });
+
+//create an appointment
+app.post('/api/v1/appointments', (req, res) => {
+  const keys = [ 'doctor', 'proficiency', 'pacient', 'time', 'date'];
+
+  for(let index = 0; index < keys.length; index++) {
+    console.log(keys[index]);
+    if(!req.body[keys[index]]) {
+      return res.status(400).send({
+        success: 'false',
+        message: `${keys[index]} is required`
+      });
+    }
+  }
+
+  let appointment = {
+    id: db.length + 1,
+    doctor: req.body.doctor,
+    proficiency: req.body.proficiency,
+    pacient: req.body.pacient,
+    time: req.body.time,
+    date: req.body.date
+  }
+
+  db.push(appointment);
+  return res.status(200).send({
+    success: 'true',
+    message: 'appointment created with success',
+    appointment
+  })
+});
+
+//getting just one appointment
+app.get('api/v1/appointments/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((appointment) => {
+    
+  })
+})
 
 const port = 3001;
 
