b94134c4500f5ed9a2e3dd20571f5cc2294517ee
Vinicius Leite <vini_cavalcanteee@hotmail.com>
2019-07-04 11:08:20 -0300

    Fix typo

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,7 +24,7 @@ router.post('/api/v1/medical_agreements', controller_ma.buildMedicalAgreement);
 //UPDATE a medical agreement
 router.put('/api/v1/medical_agreements/:id', controller_ma.modifyMedicalAgreement);
 
-//DELETE one appointment from database
+//DELETE one medical_agreement from database
 router.delete('/api/v1/medical_agreements/:id', controller_ma.excludeMedicalAgreement);
 
 /**********************************************
@@ -34,70 +34,70 @@ Controller: Doctors
 //GET all doctors
 router.get('/api/v1/doctors',  controller_doc.allDoctors);
 
-//GET just one medical agreement
+//GET just one doctor
 router.get('/api/v1/doctors/:id', controller_doc.oneDoctor);
 
-// POST a medical agreement
+// POST a doctor
 router.post('/api/v1/doctors', controller_doc.buildDoctor);
 
-//UPDATE a medical agreement
+//UPDATE a doctor
 router.put('/api/v1/doctors/:id', controller_doc.modifyDoctor);
 
-//DELETE one appointment from database
+//DELETE one doctor from database
 router.delete('/api/v1/doctors/:id', controller_doc.excludeDoctor);
 
 /**********************************************
 Controller: Offices
 ***********************************************/
 
-//GET all medical agreements
+//GET all offices
 router.get('/api/v1/offices',  controller_off.allOffices);
 
-//GET just one medical agreement
+//GET just one office
 router.get('/api/v1/offices/:id', controller_off.oneOffice);
 
-// POST a medical agreement
+// POST a office
 router.post('/api/v1/offices', controller_off.buildOffice);
 
-//UPDATE a medical agreement
+//UPDATE a office
 router.put('/api/v1/offices/:id', controller_off.modifyOffice);
 
-//DELETE one appointment from database
+//DELETE one office from database
 router.delete('/api/v1/offices/:id', controller_off.excludeOffice);
 
 /**********************************************
 Controller: Pacients
 ***********************************************/
 
-//GET all medical agreements
+//GET all pacients
 router.get('/api/v1/pacients',  controller_pac.Pacients);
 
-//GET just one medical agreement
+//GET just one pacient
 router.get('/api/v1/pacients/:id', controller_pac.onePacient);
 
-// POST a medical agreement
+// POST a pacient
 router.post('/api/v1/pacients', controller_pac.buildPacient);
 
-//UPDATE a medical agreement
+//UPDATE a pacient
 router.put('/api/v1/pacients/:id', controller_pac.modifyPacient);
 
-//DELETE one appointment from database
+//DELETE one pacient from database
 router.delete('/api/v1/pacients/:id', controller_pac.excludePacient);
 
 /**********************************************
 Controller: Appointments
 ***********************************************/
 
-//GET all medical agreements
+//GET all appointments
 router.get('/api/v1/appointments',  controller_app.allAppointments);
 
-//GET just one medical agreement
+//GET just one appointment
 router.get('/api/v1/appointments/:id', controller_app.oneAppointment);
 
-// POST a medical agreement
+// POST a appointment
 router.post('/api/v1/appointments', controller_app.buildAppointment);
 
-//UPDATE a medical agreement
+//UPDATE a appointment
 router.put('/api/v1/appointments/:id', controller_app.modifyAppointment);
 
 //DELETE one appointment from database
