2bb5180b537533a931d020f8df301d267b7a4faa
Vinicius Cavalcante Leite <vini_cavalcanteee@hotmail.com>
2019-07-30 10:57:25 -0300

    Port redefinition

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-app.listen(port || 5000, () => {
+app.listen(port , () => {
   console.log(`Server running on port ${port}`)
 });
