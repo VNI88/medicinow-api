9072913d560b3bb1867c6ef47de01cd473093f3b
Vinicius Cavalcante Leite <vini_cavalcanteee@hotmail.com>
2019-09-05 16:50:30 -0300

    Query update

--- src/routes/index.js
+++ src/routes/index.js
@@ -60,7 +60,7 @@ Controller: Offices
 router.get('/api/v1/offices',  offices.index);
 
 //GET just one office
-router.get('/api/v1/offices/:street_address', auth.verifyToken, offices.show);
+router.get('/api/v1/offices/:office_id', auth.verifyToken, offices.show);
 
 // POST a office
 router.post('/api/v1/offices', offices.create);
